21884cf5de5c60cc7918d6d7763789e9e31ebdb2
faizal <faizalramanda@gmail.com>
2018-06-29 17:51:23 +0700

    scraping news done

--- server/routes/scraping.js
+++ server/routes/scraping.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 const {
   tweetScrape,
-  fbScrape
+  fbScrape,
+  newsScrape
 } = require('../controllers/scraping.controller')
 
 router.post('/tweet', tweetScrape)
 router.post('/fb', fbScrape)
+router.post('/news', newsScrape)
 
 module.exports = router
