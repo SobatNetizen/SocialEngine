52a3383b27dc6e1a559764551eb8dc4d3df0aaca
marcosumali <marco.sumali90@gmail.com>
2018-07-01 02:06:01 +0700

    revise feature backend add demigraphy information from facebook and url link post social media

--- server/routes/scraping.js
+++ server/routes/scraping.js
@@ -3,12 +3,14 @@ const {
   tweetScrape,
   fbScrape,
   tweetScrapeProfile,
+  fbScrapeProfile,
   newsScrape
 } = require('../controllers/scraping.controller')
 
 router.post('/tweet', tweetScrape)
 router.post('/tweet/&profile', tweetScrapeProfile)
 router.post('/fb', fbScrape)
+router.post('/fb/&profile', fbScrapeProfile)
 router.post('/news', newsScrape)
 
 module.exports = router
